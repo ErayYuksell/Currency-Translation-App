98d6955b6ab126d09f545fcfbeefe1121f7e9578
Eray Yüksel <yukseleray203@gmail.com>
2022-07-25 10:39:41 +0300

    Döviz Kurlari Hesaplandi

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const api_key="16c744152bdd9ec4f88c177d";
-const url="https://v6.exchangerate-api.com/v6/" + api_key;
+const url = "https://v6.exchangerate-api.com/v6/" + api_key;
 
-
-//elements
-
-const currency_one=document.getElementById("currency_one");
-const currency_two=document.getElementById("currency_two");
-const list_one=document.getElementById("list_one");
-const list_two=document.getElementById("list_two");
-const amount=document.getElementById("amount");
-const calculate=document.getElementById("calculate");
-const result=document.getElementById("result");
+// elements
+const currency_one = document.getElementById("currency_one");
+const currency_two = document.getElementById("currency_two");
+const list_one = document.getElementById("list_one");
+const list_two = document.getElementById("list_two");
+const amount = document.getElementById("amount");
+const calculate = document.getElementById("calculate");
+const result = document.getElementById("result");
 
 fetch(url + "/codes")
     .then(res => res.json())
@@ -23,5 +21,24 @@ fetch(url + "/codes")
         }
         list_one.innerHTML = options;
         list_two.innerHTML = options;
-    })
+    });
 
+calculate.addEventListener("click", function(){
+    const doviz1 = currency_one.value;
+    const doviz2 = currency_two.value;
+    const miktar = amount.value;
+
+    fetch(url + "/latest/" + doviz1)
+        .then(res => res.json())
+        .then(data => {
+            const sonuc = (data.conversion_rates[doviz2] * miktar).toFixed(3);
+            result.innerHTML = `
+                <div class="card border-primary">
+                    <div class="card-body text-center" style="font-size:30px;" >
+                        ${miktar} ${doviz1} = ${sonuc} ${doviz2}
+                    </div>
+                </div>
+            `;
+        })
+
+});
